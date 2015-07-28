880b9339aea48744bcc90a7b75f895e595b772ac
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-28 14:54:20 -0300

    deploy changes

--- Azkfile.js
+++ Azkfile.js
@@ -6,54 +6,60 @@
 systems({
   azkdemo: {
     // Dependent systems
-    depends: ["redis"],
+    depends: ['redis'],
     // More images:  http://images.azk.io
-    image: {"docker": "azukiapp/node:0.12"},
+    image: {'docker': 'azukiapp/node:0.12'},
     // Steps to execute before running instances
     provision: [
-      "npm install",
+      'npm install',
     ],
-    workdir: "/azk/#{manifest.dir}",
-    shell: "/bin/bash",
-    command: "npm start",
+    workdir: '/azk/#{manifest.dir}',
+    shell: '/bin/bash',
+    command: 'npm start',
     wait: 20,
     mounts: {
-      '/azk/#{manifest.dir}': path("."),
+      '/azk/#{manifest.dir}': path('.'),
     },
-    scalable: {"default": 2},
+    scalable: {'default': 1},
     http: {
       domains: [
-        "#{system.name}.#{azk.default_domain}",
-        "#{process.env.AZK_HOST_IP}"
+        '#{system.name}.#{azk.default_domain}',
+        '#{process.env.AZK_HOST_IP}'
       ]
+    },
+    ports: {
+      'http': '3000/tcp'
     },
     envs: {
       // set instances variables
-      NODE_ENV: "dev",
+      NODE_ENV: 'dev',
     },
   },
-  // Adds the "redis" system
+  // Adds the 'redis' system
   redis: {
-    image: "redis",
+    image: {docker: 'redis'},
     // <-- add command and mounts
-    command: "redis-server --appendonly yes",
+    command: 'redis-server --appendonly yes',
     mounts: {
-      "/data": persistent("data"),
+      '/data': persistent('data'),
     },
     export_envs: {
-      "DATABASE_URL": "redis://#{net.host}:#{net.port[6379]}"
+      'DATABASE_URL': 'redis://#{net.host}:#{net.port[6379]}'
     }
   },
 
-  deploy: {
-    image: {"docker": "azukiapp/deploy"},
-    mounts: {
-      "/azk/deploy/src" : path("."),
-      "/azk/deploy/.ssh": path("#{process.env.HOME}/.ssh"),
-    },
-    scalable: {"default": 0, "limit": 0},
-  },
 
+  /**
+   * deploys
+   */
+  deploy: {
+    image: {'docker': 'azukiapp/deploy'},
+    mounts: {
+      '/azk/deploy/src' : path('.'),
+      '/azk/deploy/.ssh': path('#{process.env.HOME}/.ssh'),
+    },
+    scalable: {'default': 0, 'limit': 0},
+  },
   'deploy-fast': {
     extends: 'deploy',
     envs: {
